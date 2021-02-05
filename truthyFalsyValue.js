e241511f3d215a0a6283fd83ff6286ce17d9d6e6
Samin1615088 <samin.shafkat@gmail.com>
2021-02-06 00:10:19 +0600

    truthyFalsyValue file added

---
+++ truthyFalsyValue.js
@@ -0,0 +1,35 @@
+if (false) {
+    console.log("in false");
+} else {
+    console.log("not in false");
+}
+
+if (0) {
+    console.log("in 0");
+} else {
+    console.log("not in 0");
+}
+
+if ("") {
+    console.log("in qutation");
+} else {
+    console.log("not in qutation");
+}
+
+if (null) {
+    console.log("in null");
+} else {
+    console.log("not in null");
+}
+
+if (undefined) {
+    console.log("in undefined");
+} else {
+    console.log("not in undefined");
+}
+
+if (NaN) {
+    console.log("in NaN");
+} else {
+    console.log("not in NaN");
+}
