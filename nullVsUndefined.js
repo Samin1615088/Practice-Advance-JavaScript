50e71214f4646fee51bceecb1542392aa1e62d6b
Samin1615088 <samin.shafkat@gmail.com>
2021-02-06 00:36:51 +0600

    nullVsUndefined.js file is added

---
+++ nullVsUndefined.js
@@ -0,0 +1,14 @@
+function add(num1, num2) {
+    sum = num1 + num2;
+    return ;
+}
+
+
+console.log(null === undefined);
+// false
+
+console.log(null == undefined);
+// true
+
+console.log(add(12, 11));
+// undefined
